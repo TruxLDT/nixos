9a12a74ad1962436608cb1da5e482561f8309fae
trux <connormoya@gmail.com>
2026-04-08 02:09:14 -0500

    no home-manager

--- flake.nix
+++ flake.nix
@@ -6,10 +6,6 @@
     import-tree.url = "github:vic/import-tree";
 
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
